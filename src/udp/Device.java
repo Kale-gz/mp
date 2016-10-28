620f5b46e014a4b4d161dfa14881f85cc1d5ebdb
frasab <frasab89@gmail.com>
2016-10-28 16:21:42 +0200

    Nuove Modifiche protocollo invio

--- src/udp/Device.java
+++ src/udp/Device.java
@@ -2,13 +2,11 @@ package udp;
 public class Device {
     private String mac;
     private int id;
-    private int num;
     private boolean isalive;
      
     public Device (int id, int num, String mac){
         this.id = id;
         this.mac = mac;
-        this.num = num;
     }
  
     public boolean isIsalive() {
@@ -35,16 +33,4 @@ public class Device {
         this.id = id;
     }
          
-    public int getNum() {
-        return num;
-    }
- 
-    public void setNum(int num) {
-        this.num = num;
-    }
- 
-    @Override
-    public String toString() {
-        return "Device [mac=" + mac + ", id=" + id + "]";
-    }
 }
