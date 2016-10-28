856eaa51898efa88aad6ea784fc28ec679301f0f
Amedeo <amedeo.fortino@gmail.com>
2016-10-28 16:52:54 +0200

    Versione definitiva di Device, commentata

--- src/udp/Device.java
+++ src/udp/Device.java
@@ -1,36 +1,64 @@
 package udp;
+
+/**
+ * Classe contenente tutti i metodi necessari alla gestione di un Device.
+ * 
+ * @author Giuseppe De Gregorio, Amedeo Fortino, Francesca Sabatino
+ * @version 1.0.0
+ *
+ */
 public class Device {
-    private String mac;
-    private int id;
-    private boolean isalive;
-     
-    public Device (int id, int num, String mac){
-        this.id = id;
-        this.mac = mac;
-    }
- 
-    public boolean isIsalive() {
-        return isalive;
-    }
- 
-    public void setIsalive(boolean isalive) {
-        this.isalive = isalive;
-    }
- 
-    public String getMac() {
-        return mac;
-    }
- 
-    public void setMac(String mac) {
-        this.mac = mac;
-    }
- 
-    public int getId() {
-        return id;
-    }
- 
-    public void setId(int id) {
-        this.id = id;
-    }
-         
+	/** MAC address del device	*/
+	private String mac;
+	
+	/** ID del device	*/
+	private int id;
+
+	/**
+	 * Costruttore del Device.
+	 * 
+	 * @param id	Id del device
+	 * @param mac	MAC address del device
+	 */
+	public Device (int id, String mac){
+		this.id = id;
+		this.mac = mac;
+	}
+
+	/**
+	 * Restituisce il MAC address del device
+	 * 
+	 * @return	MAC address del device
+	 */
+	public String getMac() {
+		return mac;
+	}
+
+	/**
+	 * Imposta il MAC address del device
+	 * 
+	 * @param mac	mac address da impostare
+	 */
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	/**
+	 * Restituisce l'ID del device
+	 * 
+	 * @return	ID del device
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Imposta l'ID del device
+	 * 
+	 * @param id	ID del device da impostare
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
