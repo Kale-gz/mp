f8fc7befdf0ee2d1306c4a3bb80fa508ce35d7df
frasab <frasab89@gmail.com>
2016-10-28 16:10:12 +0200

    ThreadDevice implementata blablabla

---
+++ src/udp/ThreadDevice.java
@@ -0,0 +1,31 @@
+package udp;
+
+import java.util.ArrayList;
+
+public class ThreadDevice extends Thread {
+	private ArrayList<Device> deviceList;
+	private int id;
+	
+	
+	public ThreadDevice(ArrayList<Device> deviceList, int id){
+		this.deviceList=deviceList;
+		this.id=id;
+	}
+	
+	public void run(){
+		int ms_wait=5000;
+		while(true){
+			try {
+				wait(ms_wait);
+				for (Device i : deviceList){
+					if(i.getId()==id){
+						deviceList.remove(i);
+					}
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
