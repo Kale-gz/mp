6638bb74d1d1affa8f1d4ca0b87ebc089932c9ef
Amedeo <amedeo.fortino@gmail.com>
2016-10-29 12:54:00 +0200

    Thread di Francesca

--- src/udp/CheckList.java
+++ src/udp/CheckList.java
@@ -4,36 +4,56 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-
+/**
+ * Classe che implementa un thread, il cui obiettivo è gestire la ricerca dei nuovi devices connessi al server.
+ * In dettaglio ogni 5 minuti il thread invia un messaggio Multicast a tutti i devices connessi.
+ * 
+ * @author Giuseppe De Gregorio, Amedeo Fortino, Francesca Sabatino
+ * @version 1.1
+ *
+ */
 public class CheckList extends Thread{
+	/** porta a cui inviare i datagram*/
 	private int PORT;
+	
+	/** indirizzo IP */
 	private InetAddress addr;
+	
+	/** socket utilizzata per inviare i datagram*/
 	private MulticastSocket sock;
 
+	/**
+	 * Costruttore del thread
+	 * 
+	 * @param sock multicast socket del server
+	 * @param addr indirizzo ip
+	 */
 	public CheckList(MulticastSocket sock, InetAddress addr){
 		this.sock=sock;
 		this.addr = addr;
 		PORT = 7777;
 	}	
-
+	
+	/**
+	 * Metodo run del thread che richiama il metodo checkAlive() ogni 5 minuti
+	 */
 	public void run(){
-		long ms_wait=2000;
-		
-        while(true){
+		long ms_wait=300000;
+		while(true){
 			try {
 				checkAlive();
 				Thread.sleep(ms_wait);
 			} catch (InterruptedException | IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}		
 	}
-
+	
+	/**
+	 * Metodo del thread che si occupa di inviare un messaggio multicast ai devices connessi
+	 */
 	public void checkAlive() throws IOException{
-		byte[] msg={'i','s','a','l','i','v','e'};
-		//		MulticastSocket sock=new MulticastSocket();
-		//		InetAddress addr=InetAddress.getByName("0.0.0.0");
+		byte[] msg = {'i','s','a','l','i','v','e'};
 		DatagramPacket packet=new DatagramPacket(msg,msg.length,addr,PORT);
 		sock.send(packet);
 		System.out.println("Ho inviato il pacchetto Multicast");
--- src/udp/ThreadDevice.java
+++ src/udp/ThreadDevice.java
@@ -1,26 +1,57 @@
 package udp;
 
 import java.util.ArrayList;
-
+/**
+ * Classe che implementa un thread, il cui obiettivo è assicurarsi che un device sia 
+ * effettivamente connesso al server.
+ * In dettaglio se un devices non invia un pacchetto per più di 100 secondi, il thread 
+ * si occupa di eliminarlo dalla lista dei devices connessi.
+ * 
+ * @author Giuseppe De Gregorio, Amedeo Fortino, Francesca Sabatino
+ * @version 1.2
+ *
+ */
 public class ThreadDevice extends Thread {
+	/** Lista dei device connessi*/
 	private ArrayList<Device> deviceList;
-	private int id;
-	boolean restart;
-	 
 	
+	/**ID del device*/
+	private int id;
+	
+	/** Flag che mi permette di far ripartire il thread all'arrivo di un nuovo pacchetto "imalive" */
+	boolean restart;
+
+	/**
+	 * Costruttore della classe
+	 * 
+	 * @param deviceList lista dei device
+	 * @param id id del device
+	 */
 	public ThreadDevice(ArrayList<Device> deviceList, int id){
 		this.deviceList=deviceList;
 		this.id=id;
 		restart=false;
 	}
+	
+	/**
+	 * Metodo per ottenere l'id del device
+	 * @return id
+	 */
 	public int getMyId(){
 		return id;
 	}
+	
+	/**
+	 * Metodo run del thread. Il thread conta 100 secondi, durante i quali:
+	 * - se non riceve pacchetti dal device, tramite il flag restart, lo rimuove dalla lista. 
+	 * - ogni volta che riceve pacchetti dal device, riparte il conteggio.
+	 */
 	public void run(){
-		long ms_wait=1000;
+		long ms_wait = 1000;
 		int sec = 0;
-		int max_sec = 30;
+		int max_sec = 100;
 		Device ndev = new Device();
+
 		while(sec<max_sec){
 			try {
 				sec++;
@@ -31,7 +62,6 @@ public class ThreadDevice extends Thread {
 				}
 				Thread.sleep(ms_wait);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -44,6 +74,9 @@ public class ThreadDevice extends Thread {
 		System.out.println("Ho eliminato il device "+id+"  dalla lista.");
 	}
 
+	/**
+	 * Metodo che setta il flag restart opportunamente al fine di poter eseguire il restart del thread
+	 */
 	public void doRestart(){
 		restart = true;
 	}
